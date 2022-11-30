c0cafdd3577160639b3ce18df80b73a9d4a6dcb7
Juan José Aroca Ariza <juanjose.aroca@utp.edu.co>
2022-11-29 21:21:25 -0500

    production commit

--- src/routes/router.js
+++ src/routes/router.js
@@ -30,4 +30,4 @@ process.on("unhandledRejection", err => {
 })
 
 // server initialization
-app.listen(procces.ENV.PORT, () => console.log('Server is running'));
+app.listen(procces.env.PORT, () => console.log('Server is running'));
