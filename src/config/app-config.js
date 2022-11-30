ecf3553b7144d9106cf7274228cd7bcaf56ff7d6
Juan José Aroca Ariza <juanjose.aroca@utp.edu.co>
2022-11-29 21:18:53 -0500

    cambios produccion 3

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -6,19 +6,19 @@ const config = {
     'views' : path.join(__dirname , '/../views'),
     'controllers' : path.join(__dirname , '/../controllers'),
     'sqlCon' : {
-        host: process.ENV.DB_HOST,
-        user: process.ENV.DB_USER,
-        password: process.ENV.DB_PASS,
-        database: process.ENV.DB_NAME,
-        port: process.ENV.DB_PORT,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
         charset : 'utf8mb4'
     },
     'populateCon' : {
-        host: process.ENV.DB_HOST,
-        user: process.ENV.DB_USER,
-        password: process.ENV.DB_PASS,
-        database: process.ENV.DB_NAME,
-        port: process.ENV.DB_PORT,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
         charset : 'utf8mb4',
         multipleStatements: true
     }
