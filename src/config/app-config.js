ec83658ace7976b71ee65e2daadf8bd47f157f0d
Juan José Aroca Ariza <juanjose.aroca@utp.edu.co>
2022-11-30 09:10:33 -0500

    production commit 17

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -6,10 +6,11 @@ const config = {
     'views' : path.join(__dirname , '/../views'),
     'controllers' : path.join(__dirname , '/../controllers'),
     'sqlCon' : {
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASS || '',
-        database: process.env.DB_NAME || 'lib',
+
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         port: process.env.DB_PORT,
         charset : 'utf8mb4'
     },
